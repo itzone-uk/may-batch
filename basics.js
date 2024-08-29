8472d6215eaf30ad2a81349ad645d68e22416872
PirateCoderz <118294175+PirateCoderz@users.noreply.github.com>
2024-08-29 15:19:25 +0500

    Create basics.js

---
+++ basics.js
@@ -0,0 +1,43 @@
+console.log("JS is Running")
+
+
+// premitive datatypes
+// let const var
+
+var a = 10; // integer
+
+var b = 3.14; // float
+
+var char = "$32rewq"; // char
+
+var c = "Hello World"; // string
+ 
+var d = true; // boolean
+
+var e = null; // null
+
+var f = undefined; // undefined
+
+// memory allocation
+
+console.log("a:", a, typeof a); // a: 10 number
+
+console.log("b:", b, typeof b); // b: 3.14 number
+
+console.log("name:", name, typeof name); // name: $ string
+
+console.log("c:", c, typeof c); // c: Hello World string
+
+
+// non premitive datatypes
+
+var g = [1, 2, 3, 4, 5]; // array
+
+var h = { name: "John", age: 30 }; // object
+
+var j = function() {
+  console.log("Hello World");
+}; // function
+
+
+var i = new Date(); // Date object
